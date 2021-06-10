b535b8a2010d4897db700c2e49787895aca834ab
Abrar <ahmedh6242@gmail.com>
2021-06-10 05:25:37 -0400

    workshop 3 part 2 started

--- WS03/w3/Pair.h
+++ WS03/w3/Pair.h
@@ -14,12 +14,12 @@ namespace sdds {
 		}
 		const V& value() const { return m_value; }
 		const K& key()const { return m_key; }
-		void display(std::ostream& os)const {
+		virtual void display(std::ostream& os)const {
 			os << m_key << " : " << m_value << std::endl;
 		}
 	};
 	template<typename V, typename K>
-	virtual std::ostream& operator<<(std::ostream& os, const Pair<V, K>& pair) {
+	std::ostream& operator<<(std::ostream& os, const Pair<V, K>& pair) {
 		pair.display(std::cout);
 		return os;
 	}
--- WS03/w3/PairSummable.h
+++ WS03/w3/PairSummable.h
@@ -3,15 +3,26 @@
 #include "Pair.h"
 namespace sdds {
 	template<typename V, typename K>
-	class PairSummable : Pair<V,K>{
+	class PairSummable : pair<V,K>{
 		V m_initial_value;
 		size_t m_width = 0;
 	public:
 		PairSummable() {}
-		PairSummable(const k& key, const v& value = initial) {
-
+		PairSummable(const K& key, const V& value = initial) {
+			Pair<int, std::string>(key, value);
+			k.size() != 0 && m_width = k.size();
 		}
-		bool isCompatibleWith(const PairSummable<V,K>& b)const{}
+		bool iscompatiblewith(const PairSummable<V,K>& b)const{
+			return m_key == b.m_key;
+		}
+		PairSummable<V,K>& operator+=(cosnt PairSummable<V, K>& b) {
+			m_initial_value = b.m_initial_value;
+		}
+		virtual void display(std::ostream& os)const {
+			os.width(m_width);
+			os.setf(ios::left);
+			display(os);
+		}
 	};
 }
 #endif // !_SDDS_PAIRSUMMABLE_H
